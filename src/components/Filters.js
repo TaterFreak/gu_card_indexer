84035bab5326dc07df7a95913caee0d94084556d
TaterFreak <marguetjames@gmail.com>
2019-05-11 11:26:44 +0200

    improving filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -11,38 +11,58 @@ class App extends React.Component {
 
   filter(params) {
     if (params.hasOwnProperty('rarity')) {
-      if (-1 === this.state.toggled.indexOf(params.rarity)) {
-        this.state.toggled.push(params.rarity)
+      if (-1 === Object.values(this.state.toggled).indexOf(params.rarity)) {
+        this.setState({
+          toggled: params
+        })
+        //this.state.toggled.push(params.rarity)
       } else {
-        const index = this.state.toggled.indexOf(params.rarity)
-        this.state.toggled.splice(index, 1)
+        const index = Object.values(this.state.toggled).indexOf(params.rarity)
+        this.setState({
+          toggled: {}
+        })
       }
     }
 
     if (params.hasOwnProperty('quality')) {
-      if (-1 === this.state.toggled.indexOf(params.quality)) {
-        this.state.toggled.push(params.quality)
+      if (-1 === Object.values(this.state.toggled).indexOf(params.quality)) {
+        this.setState({
+          toggled: params
+        })
+        //this.state.toggled.push(params.rarity)
       } else {
-        const index = this.state.toggled.indexOf(params.quality)
-        this.state.toggled.splice(index, 1)
+        const index = Object.values(this.state.toggled).indexOf(params.quality)
+        this.setState({
+          toggled: {}
+        })
       }
-    }
 
-    if (params.hasOwnProperty('type')) {
-      if (-1 === this.state.toggled.indexOf(params.type)) {
-        this.state.toggled.push(params.type)
-      } else {
-        const index = this.state.toggled.indexOf(params.type)
-        this.state.toggled.splice(index, 1)
+      if (params.hasOwnProperty('type')) {
+        if (-1 === Object.values(this.state.toggled).indexOf(params.type)) {
+          this.setState({
+            toggled: params
+          })
+          //this.state.toggled.push(params.rarity)
+        } else {
+          const index = Object.values(this.state.toggled).indexOf(params.type)
+          this.setState({
+            toggled: {}
+          })
+        }
       }
-    }
 
-    if (params.hasOwnProperty('tribe')) {
-      if (-1 === this.state.toggled.indexOf(params.tribe)) {
-        this.state.toggled.push(params.tribe)
-      } else {
-        const index = this.state.toggled.indexOf(params.tribe)
-        this.state.toggled.splice(index, 1)
+      if (params.hasOwnProperty('tribe')) {
+        if (-1 === Object.values(this.state.toggled).indexOf(params.tribe)) {
+          this.setState({
+            toggled: params
+          })
+          //this.state.toggled.push(params.rarity)
+        } else {
+          const index = Object.values(this.state.toggled).indexOf(params.tribe)
+          this.setState({
+            toggled: {}
+          })
+        }
       }
     }
 
@@ -50,24 +70,25 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(this.state)
     return (
       <div>
         <button onClick={() => this.filter({'': ''})}>clear</button>
         <hr />
         {RARITIES.map((rarity, index) =>
-          <button onClick={() => this.filter({rarity: rarity})} className={this.state.toggled.indexOf(rarity) != -1 ? 'toggled' : ''}>{rarity}</button>
+          <button onClick={() => this.filter({rarity: rarity})} className={Object.values(this.state.toggled).indexOf(rarity) != -1 ? 'toggled' : ''}>{rarity}</button>
         )}
         <hr />
         {QUALITIES.map((quality, index) =>
-          <button onClick={() => this.filter({quality: quality})} className={this.state.toggled.indexOf(quality) != -1 ? 'toggled' : ''}>{quality}</button>
+          <button onClick={() => this.filter({quality: quality})} className={Object.values(this.state.toggled).indexOf(quality) != -1 ? 'toggled' : ''}>{quality}</button>
         )}
         <hr />
         {TYPES.map((type, index) =>
-          <button onClick={() => this.filter({type: type})} className={this.state.toggled.indexOf(type) != -1 ? 'toggled' : ''}>{type}</button>
+          <button onClick={() => this.filter({type: type})} className={Object.values(this.state.toggled).indexOf(type) != -1 ? 'toggled' : ''}>{type}</button>
         )}
         <hr />
         {TRIBES.map((tribe, index) =>
-          <button onClick={() => this.filter({tribe: tribe})} className={this.state.toggled.indexOf(tribe) != -1 ? 'toggled' : ''}>{tribe}</button>
+          <button onClick={() => this.filter({tribe: tribe})} className={Object.values(this.state.toggled).indexOf(tribe) != -1 ? 'toggled' : ''}>{tribe}</button>
         )}
         <style jsx>{`
           .toggled {
